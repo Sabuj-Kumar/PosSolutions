9da55ae51a1016b8b6b782ecc5145dd766f3a6f8
sabuj_kumar <sabuj.job21@gmail.com>
2023-02-23 01:25:31 +0600

    added model and controller

---
+++ PosSolution/Controllers/ItemController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace PosSolution.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IConfiguration _configuration { get; }
+
+        public ItemController(IConfiguration configuration)
+        {
+            this._configuration = configuration; 
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(_configuration.GetConnectionString("DbConnection"))) { 
+                    
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // Get: ItemController/Edit/5
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetAllItem()
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ PosSolution/Models/ItemModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PosSolution.Models
+{
+    public class ItemModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Insert Item Name")]
+        [DisplayName("Item")]
+        public string item { get; set; }
+        [Required(ErrorMessage = "Enter Quentity Please")]
+        [DisplayName("Quentity")]
+        public int quentity { get; set; }
+        [Required(ErrorMessage = "Enter Price Please")]
+        [DisplayName("Price")]
+        public float price { get; set; }
+        [Required(ErrorMessage = "Enter Amount Please")]
+        [Display(Name = "Amount")]
+        public float amount { get; set; }
+    }
+}
